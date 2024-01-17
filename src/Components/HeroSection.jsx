a35e21559e2400ff3573ec8dc7a2d6badb8be3d7
Rohan-2107 <rohanrkc289@gmail.com>
2024-01-17 21:31:33 +0530

    Added Styling to About and Contact section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -3,15 +3,10 @@ import React from "react";
 
 const HeroSection = () => {
     return (
-        <div className="main-page">
-            <div className="main-container">
+        <div className=' bg-slate-300'>
+            <div>
                 <div className='w-full h-screen bg-cover'>
                     <img className='w-full h-screen bg-cover bg-fixed' src="./public/image2.jpeg" alt="main" />
-                </div>
-                <div className="main-text">
-                    {/* introduction text */}
-                    Welcome to 
-                    <p className="main-text-2">RoxWealth</p>
                 </div>
             </div>
         </div>
